d1bad3bcfbb5f3977210e8f14ec16609b9325f1f
shruti <pandiyashruti05@gmail.com>
2020-12-28 14:33:58 +0530

    My Django Girls app, first commit

---
+++ f1.py
@@ -0,0 +1,14 @@
+def hi():
+    print('hello world')
+    return 0
+
+def name(username):
+    print('username is ',username)
+    return 0
+a=['s','h','r','u','t','i']
+for i in a:
+    print(i)
+for j in range(0,3):
+    print(j)
+hi()
+name('shruti')
